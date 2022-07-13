cb05901e20be4367142169141d322ce4684106d2
Beyza Topcu <beyza.topcu@bilgeadam.com>
2022-07-13 19:48:47 +0300

    created a dummy home screen

--- src/7_Modules/Home/index.ts
+++ src/7_Modules/Home/index.ts
@@ -1,2 +1,8 @@
+import HomeScreen from './Screens/HomeScreen';
+
 export { type HomeRouteParams } from './Types/RouteParams';
 export { default as HomeScreenNames } from './Constants/ScreenNames';
+
+export const HomeScreens = {
+    HomeScreen,
+};

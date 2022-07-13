efebf3d6e4813aeb43cac78bfab624ce48881cee
Beyza Topcu <beyza.topcu@bilgeadam.com>
2022-07-13 08:48:54 +0300

    global navigation constants and types

---
+++ src/4_Navigation/Constants/ScreenNames.ts
@@ -0,0 +1,7 @@
+import { EntriesScreensNames } from '7_Modules/Entries';
+import { HomeScreenNames } from '7_Modules/Home';
+
+export default {
+    ...EntriesScreensNames,
+    ...HomeScreenNames,
+};
---
+++ src/4_Navigation/Types/NavigationTypes.ts
@@ -0,0 +1,4 @@
+import { EntriesRouteParams } from '7_Modules/Entries';
+import { HomeRouteParams } from '7_Modules/Home';
+
+export type RootStackParamList =  HomeRouteParams | EntriesRouteParams;

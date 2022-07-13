3ae6c4599c3893e5fbdc77b626d4f01b4f39f760
Beyza Topcu <beyza.topcu@bilgeadam.com>
2022-07-13 23:04:48 +0300

    adjusted hitslop for search button

--- src/4_Navigation/Components/HeaderRight.tsx
+++ src/4_Navigation/Components/HeaderRight.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
+import Metrics from '../../0_Constants/Metrics';
 import Search from '../../1_Assets/Svgs/Search.svg';
 
 const HeaderRight = () : React.ReactNode => {
     return (
-        <TouchableOpacity testID="search-button">
-            <Search />
+        <TouchableOpacity
+            testID="search-button"
+            hitSlop={{
+                left: Metrics.measure(30),
+                right: Metrics.measure(10),
+                bottom: Metrics.measure(10),
+                top: Metrics.measure(10),
+            }}>
+            <Search
+                width={Metrics.measure(20)}
+                height={Metrics.measure(20)} />
         </TouchableOpacity>
     );
 };

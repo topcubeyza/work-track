60ad517365ae15d3a81aa1d5c739694f3466b8b9
Beyza Topcu <beyza.topcu@bilgeadam.com>
2022-07-13 08:48:42 +0300

    updated home screen names

--- src/7_Modules/Home/Constants/ScreenNames.ts
+++ src/7_Modules/Home/Constants/ScreenNames.ts
@@ -1,8 +1,10 @@
 const MODULE_NAME = 'home';
 
-export default {
+const ScreenNames = {
     Home: MODULE_NAME + '/home',
     ProjectList: MODULE_NAME + '/project-list',
     TagList: MODULE_NAME + '/tag-list',
     Calendar: MODULE_NAME + '/calendar',
 };
+
+export default ScreenNames;

0464f26a19c60917b38e81b0da281dfcc72de8e5
Beyza Topcu <beyza.topcu@bilgeadam.com>
2022-07-13 08:49:09 +0300

    added root stack navigation

--- src/4_Navigation/RootStackNavigator.tsx
+++ src/4_Navigation/RootStackNavigator.tsx
@@ -1,3 +1,18 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 import { View } from 'react-native';
 
-export default View;
+const RootStackNavigator = createStackNavigator();
+
+const RootStackNavigation = () => {
+    return (
+        <RootStackNavigator.Navigator
+            id="root-stack">
+            <RootStackNavigator.Screen
+                component={View}
+                name="hello"/>
+        </RootStackNavigator.Navigator>
+    );
+};
+
+export default RootStackNavigation;
